3e7214a308274ca4fcd2a7b0ad50f835ad66be1c
Thibaut ROUTHE <e2104605@ensimlan.univ-lemans.fr>
2023-01-18 11:43:34 +0100

    Initial commit

---
+++ src/main/java/fr/ensim/info/zoo/IZooService.java
@@ -0,0 +1,15 @@
+package fr.ensim.info.zoo;
+
+import fr.ensim.info.zoo.animaux.Animal;
+import fr.ensim.info.zoo.exceptions.LimiteVisiteurException;
+
+import java.util.List;
+
+public interface IZooService {
+    public void ajouterSecteur(TypeAnimal ta);
+    public void nouveauVisiteur() throws LimiteVisiteurException;
+    public int getLimiteVisiteur();
+    public void nouvelAnimal(Animal a);
+    public int nombreAnimaux();
+    public List<Secteur> getSecteursAnimaux();
+}
---
+++ src/main/java/fr/ensim/info/zoo/ZooService.java
@@ -0,0 +1,39 @@
+package fr.ensim.info.zoo;
+
+import fr.ensim.info.zoo.animaux.Animal;
+import fr.ensim.info.zoo.exceptions.LimiteVisiteurException;
+
+import java.util.List;
+
+public class ZooService implements IZooService{
+    IZooDaO zoodao;
+    @Override
+    public void ajouterSecteur(TypeAnimal ta) {
+        zoodao.ajouterSecteur(ta);
+    }
+
+    @Override
+    public void nouveauVisiteur() throws LimiteVisiteurException {
+        zoodao.nouveauVisiteur();
+    }
+
+    @Override
+    public int getLimiteVisiteur() {
+        return zoodao.getLimiteVisiteur();
+    }
+
+    @Override
+    public void nouvelAnimal(Animal a) {
+        zoodao.getLimiteVisiteur();
+    }
+
+    @Override
+    public int nombreAnimaux() {
+        return zoodao.nombreAnimaux();
+    }
+
+    @Override
+    public List<Secteur> getSecteursAnimaux() {
+        return zoodao.getSecteursAnimaux();
+    }
+}
